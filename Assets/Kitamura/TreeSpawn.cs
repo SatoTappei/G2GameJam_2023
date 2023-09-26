9f8af24602950f8cf8d454e2b6a7ea7cea3a5a49
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2023-09-26 17:17:24 +0900

    [update]木がスポーンできない問題

--- Assets/Kitamura/TreeSpawn.cs
+++ Assets/Kitamura/TreeSpawn.cs
@@ -7,12 +7,7 @@ public class TreeSpawn : MonoBehaviour
     [SerializeField] SpriteRenderer _tree;
     [SerializeField] Transform[] _spawn;
     BackGroundScroll _backGroundScroll;
-    float _timer;
-    [Header("ÉMÉAÇ≤Ç∆ÇÃÉXÉ|Å[Éìïpìx(ïb)")]
-    [SerializeField] float _gearOne = 0.5f;
-    [SerializeField] float _gearTwo = 0.3f;
-    [SerializeField] float _gearThree = 0.1f;
-
+    float timer;
     void Start()
     {
         _backGroundScroll = GameObject.Find("System").GetComponent<BackGroundScroll>();
@@ -20,22 +15,11 @@ public class TreeSpawn : MonoBehaviour
     void Update()
     {
         float gear = _backGroundScroll.ScrollSpeed;
-        _timer += Time.deltaTime;
-        if (gear == 1)
+        timer += Time.deltaTime;
+        if (timer > 0.5f)
         {
-            if (_timer > 0.5f)
-            {
-                _timer = 0;
-                Instantiate(_tree, _spawn[Random.Range(0, 4)].position, transform.rotation);
-            }
-        }
-        else if (gear == 2)
-        {
-            if (_timer > 0.5f)
-            {
-                _timer = 0;
-                Instantiate(_tree, _spawn[Random.Range(0, 4)].position, transform.rotation);
-            }
+            timer = 0;
+            Instantiate(_tree, _spawn[Random.Range(0, 4)].position,transform.rotation);
         }
     }
 }
